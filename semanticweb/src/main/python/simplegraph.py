302d3806c92d8ca3fa62af58854df6429c694347
Debmalya Jash <debmalya.jash@gmail.com>
2016-01-05 06:15:59 +0800

    Adding to index

--- semanticweb/src/main/python/simplegraph.py
+++ semanticweb/src/main/python/simplegraph.py
@@ -8,4 +8,12 @@ class SimpleGraph:
 		self.pos = {}
 		self.osp = {}
 
-
+	'''
+	Adding to index
+	'''
+	def _addToIndex(self,index,a,b,c):
+		if a not in index: index[a] = {b:set([c])}
+		else:
+			if b not in index[a]: index[a][b] = set([c])
+			else: 
+				index[a][b].add(c)
